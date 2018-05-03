07b877e763288c66b224da687f80e772226a07df
Surgie Finesse <finesserus@gmail.com>
2018-05-03 19:18:01 +1000

    Switch the tests back to jsdom because phantomjs sucks

--- test/reactFriendlyInputTest.js
+++ test/reactFriendlyInputTest.js
@@ -1,138 +1,121 @@
-const expect = chai.expect;
+const {readFileSync} = require('fs');
+const {expect} = require('chai');
+const {JSDOM} = require('jsdom');
 
-// This test expects to be run in a browser. That's why it is written on ES5. Open runner.html to run it.
-describe('Tests reactFriendlyInput', function () {
+describe('Tests reactFriendlyInput', () => {
 
-	const reactVersions = [
-		{
-			version: '0.14',
+	const reactVersions = {
+		0.14: {
 			reactUrl: 'https://cdn.jsdelivr.net/npm/react@0.14/dist/react.min.js',
 			reactDomUrl: 'https://cdn.jsdelivr.net/npm/react-dom@0.14/dist/react-dom.min.js'
 		},
-		{
-			version: '15',
+		15: {
 			reactUrl: 'https://cdn.jsdelivr.net/npm/react@15/dist/react.min.js',
 			reactDomUrl: 'https://cdn.jsdelivr.net/npm/react-dom@15/dist/react-dom.min.js'
 		},
-		{
-			version: '16',
+		16: {
 			reactUrl: 'https://cdn.jsdelivr.net/npm/react@16/umd/react.production.min.js',
 			reactDomUrl: 'https://cdn.jsdelivr.net/npm/react-dom@16/umd/react-dom.production.min.js'
 		}
-	];
+	};
 
-	const app = document.getElementById('app');
+	const reactFriendlyInputCode = readFileSync(__dirname + '/../dist/react-friendly-input.umd.js');
 
-	// Run the tests in the browser environment with each supported React version
-	reactVersions.forEach(function (reactVersion) {
-		const version = reactVersion.version;
-		const reactUrl = reactVersion.reactUrl;
-		const reactDomUrl = reactVersion.reactDomUrl;
+	// Run each test in the browser environment with every supported React version
+	for (const [version, {reactUrl, reactDomUrl}] of Object.entries(reactVersions)) {
+		describe(`React v${version}`, () => {
+			let window, view;
 
-		describe('Tests with React v' + version, function () {
-			before('load the resources', function (done) {
+			before('init a DOM and load React', async function () {
 				this.timeout(10000);
-				loadScriptsSync([reactUrl, reactDomUrl, '../dist/react-friendly-input.umd.js'], done);
+
+				const DOM = new JSDOM(`<!DOCTYPE html>
+				<html>
+					<head>
+						<script src="${reactUrl}"></script>
+						<script src="${reactDomUrl}"></script>
+						<script>${reactFriendlyInputCode}</script>
+					</head>
+					<body>
+						<div id="app"></div>
+					</body>
+				</html>`, {
+					resources: 'usable',
+					runScripts: 'dangerously'
+				});
+				window = DOM.window;
+
+				await new Promise(resolve => {
+					if (window.document.readyState === 'complete') {
+						resolve();
+					} else {
+						window.document.addEventListener('load', resolve);
+					}
+				});
+				view = window.document.getElementById('app');
 			});
 
-			it('has loaded resources', function () {
+			it('has loaded resources', () => {
 				expect(window.React).not.to.equal(undefined);
 				expect(window.ReactDOM).not.to.equal(undefined);
 				expect(window.reactFriendlyInput).to.be.a('function');
 			});
 
-			it('has correct display name', function () {
-				expect(reactFriendlyInput('input').displayName).to.equal('reactFriendlyInput(input)');
-				expect(reactFriendlyInput('select').displayName).to.equal('reactFriendlyInput(select)');
-				//expect(reactFriendlyInput(class Foo extends window.React.Component {}).displayName).to.equal('reactFriendlyInput(Foo)');
+			it('has correct display name', () => {
+				expect(window.reactFriendlyInput('input').displayName).to.equal('reactFriendlyInput(input)');
+				expect(window.reactFriendlyInput('select').displayName).to.equal('reactFriendlyInput(select)');
+				expect(window.reactFriendlyInput(class Foo extends window.React.Component {}).displayName).to.equal('reactFriendlyInput(Foo)');
 			});
 
-			it('renders an input', function () {
-				const FriendlyInput = reactFriendlyInput('input');
-				ReactDOM.render(React.createElement(FriendlyInput, {type: 'text', value: 'initial'}), app);
+			it('renders an input', () => {
+				const FriendlyInput = window.reactFriendlyInput('input');
+				window.ReactDOM.render(window.React.createElement(FriendlyInput, {type: 'text', value: 'initial'}), view);
 
-				const input = app.firstElementChild;
+				const input = view.firstElementChild;
 				expect(input).not.to.equal('undefined');
 				expect(input.tagName).to.equal('INPUT');
 				expect(input.type).to.equal('text');
 				expect(input.value).to.equal('initial');
 			});
 
-			it('lets change the input value when the input is not focused', function () {
-				const FriendlyInput = reactFriendlyInput('input');
-				ReactDOM.render(React.createElement(FriendlyInput, {type: 'text', value: 'before focus'}), app);
-				expect(app.firstElementChild.value).to.equal('before focus');
+			it('lets change the input value through props when the input is not focused', () => {
+				const FriendlyInput = window.reactFriendlyInput('input');
+				window.ReactDOM.render(window.React.createElement(FriendlyInput, {type: 'text', value: 'before focus'}), view);
+				expect(view.firstElementChild.value).to.equal('before focus');
 			});
 
-			it('doesn\'t let change the input value when the input is focused', function () {
-				const FriendlyInput = reactFriendlyInput('input');
+			it('doesn\'t let change the input value and selection through props when the input is focused', () => {
+				const FriendlyInput = window.reactFriendlyInput('input');
 
-				// This is the only way to focus the input I've found
-				ReactDOM.render(React.createElement(FriendlyInput, {type: 'text', value: 'before focus'}), app);
-				app.firstElementChild.focus();
+				// This is the only way to focus the input I've found (it is not a fault of jsdom)
+				window.ReactDOM.render(window.React.createElement(FriendlyInput, {type: 'text', value: 'before focus'}), view);
+				let input = view.firstElementChild;
+				input.focus();
+				input.value += ' typing';
+				input.selectionStart = input.selectionEnd = 5;
 
-				ReactDOM.render(React.createElement(FriendlyInput, {type: 'text', value: 'while focus'}), app);
-				expect(app.firstElementChild.value).to.equal('before focus');
+				window.ReactDOM.render(window.React.createElement(FriendlyInput, {type: 'text', value: 'while focus'}), view);
+				input = view.firstElementChild;
+				expect(input.value).to.equal('before focus typing');
+				expect(input.selectionStart).to.equal(5);
+				expect(input.selectionEnd).to.equal(5);
 			});
 
-			it('applies the value, passed by props while the input was focused, on blur', function () {
-				const input = app.firstElementChild;
+			it('applies the value, passed through props while the input was focused, on blur', () => {
+				const input = view.firstElementChild;
+				input.value += ' more';
 				input.blur();
 				expect(input.value).to.equal('while focus');
 			});
 
-			it('unmounts the input', function () {
-				ReactDOM.unmountComponentAtNode(app);
-				expect(app.childNodes.length).to.be.equal(0);
+			it('unmounts the input', () => {
+				window.ReactDOM.unmountComponentAtNode(view);
+				expect(view.childNodes.length).to.be.equal(0);
 			});
 
-			after('unload the resources', function () {
-				delete window.React;
-				delete window.ReactDOM;
-				delete window.reactFriendlyInput;
+			after('destroy the DOM', () => {
+				window.close();
 			});
 		});
-	});
+	}
 });
-
-
-function loadScript(url, callback, document)
-{
-	if (!callback) {
-		callback = function () {};
-	}
-	if (!document) {
-		document = window.document;
-	}
-
-	const script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.async = true;
-	script.onload = function () {callback(null, script)};
-	script.onerror = callback;
-	script.src = url;
-	(document.head || document.body).appendChild(script);
-}
-
-function loadScriptsSync(urls, callback)
-{
-	if (!callback) {
-		callback = function () {};
-	}
-
-	if (!urls.length) {
-		callback();
-		return;
-	}
-
-	urls = urls.concat([]);
-	const url = urls.shift();
-
-	loadScript(url, function (error) {
-		if (error) {
-			callback(error);
-		} else {
-			loadScriptsSync(urls, callback);
-		}
-	});
-}
